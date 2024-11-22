6a63c594973403452a0d434d11d04b0f68420d64
ChandlerAimesHolt <chandleraimesholt@gmail.com>
2024-11-22 18:32:13 -0500

    Update and rename Covid SQL project.sql to m

--- Covid SQL project.sql
+++
@@ -1,7 +0,0 @@
-SELECT TOP (1000) [continent]
-      ,[location]
-      ,[date]
-      ,[population]
-      ,[new_vaccinations]
-      ,[Rolling_people_vaccinated]
-  FROM [Portfolio Project].[dbo].[percentPopulationvaccinated]
